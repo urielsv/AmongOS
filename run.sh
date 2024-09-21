867d1bafa8666390e11e1cb253391a4c22b2b0b0
Uriel Sosa Vazquez <urielsosavazquez@gmail.com>
2024-09-20 22:58:42 -0300

    Fix multiplatform qemu view and qemu run script

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 \
-    -m 512 \
-    # -soundhw pcspk
+
+# Check the operating system
+OS=$(uname)
+
+# Set the QEMU command
+QEMU_CMD="qemu-system-x86_64 -s -hda Image/x64BareBonesImage.qcow2 -m 512"
+
+# Check if the OS is Darwin (macOS)
+if [[ "$OS" == "Darwin" ]]; then
+    # Additional options for macOS can be added here
+    echo "Running QEMU on macOS..."
+else
+    # Assuming Linux/Windows (adapt for your needs)
+    QEMU_CMD="$QEMU_CMD -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0"
+    echo "Running QEMU on Linux/Windows..."
+fi
+
+# Execute the command
+echo "Executing: $QEMU_CMD"
+$QEMU_CMD
