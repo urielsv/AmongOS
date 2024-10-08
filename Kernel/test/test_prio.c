5cbcc6024c74898e910a70d0287cfd255e1e1641
felitba <feli@itba.edu.ar>
2024-10-08 19:21:04 -0300

    minor changes

--- Kernel/test/test_prio.c
+++ Kernel/test/test_prio.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "syscall.h"
-#include "test_util.h"
+#include "include/test_util.h"
 
 #define MINOR_WAIT 1000000 // TODO: Change this value to prevent a process from flooding the screen
 #define WAIT 10000000      // TODO: Change this value to make the wait long enough to see theese processes beeing run at least twice
--- Kernel/test/test_processes.c
+++ Kernel/test/test_processes.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "syscall.h"
-#include "test_util.h"
+#include "include/test_util.h"
 
 enum State { RUNNING,
              BLOCKED,
