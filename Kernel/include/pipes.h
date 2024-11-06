fd67901bac2fad7f76c2445a15069f0a4ddd00ff
felitba <feli@itba.edu.ar>
2024-11-06 19:24:38 -0300

    implementing pipes

--- Kernel/include/pipes.h
+++ Kernel/include/pipes.h
@@ -1,4 +1,5 @@
-#ifndef PIPES
+  #ifndef PIPES_H
+  #define PIPES_H
 
 #define MAX_PIPES 10
 #define PIPE_MANAGER_ADDRESS 0x80000000
@@ -7,12 +8,14 @@
 
 typedef struct pipe_manager_cdt * pipe_manager_adt;
 
+#include <stdint.h>
+
 pipe_manager_adt init_pipe_manager();
 uint16_t create_pipe();
-uint16_t open_pipe(uint16_t pipe_id);
+uint16_t open_pipe(uint16_t pipe_id, uint8_t mode);
 uint16_t close_pipe(uint16_t pipe_id);
-uint16_t write_pipe(uint16_t pipe_id, char * data, uint16_t size);
-uint16_t read_pipe(uint16_t pipe_id, char * data, uint16_t size);
+uint16_t write_pipe(uint16_t pid, uint16_t pipe_id, char * data, uint16_t size);
+uint16_t read_pipe(uint16_t pid, uint16_t pipe_id, char * data, uint16_t size);
 
 
 #endif
